cc5d9b65de07ad456aa1aa4e09b5a2e976e01a23
chrissx@ThinkBadly <chrissx@chrissx.ga>
2019-08-01 15:25:50 +0200

    added all the scripts

---
+++ gen_urls.sh
@@ -0,0 +1,1 @@
+grep '<a' beautified | sed 's/        <div class="v1Nh3 kIKUG  _bz0w">//g' | sed 's/<a href="\/p\///g' | sed 's/\/">//g' > urls
---
+++ open_privates.sh
@@ -0,0 +1,2 @@
+I=0
+for i in $(for i in $(cat private) ; do grep "$i" beautified ; done | sed 's/        <div class="v1Nh3 kIKUG  _bz0w"><a href="/https:\/\/instagram.com/g' | sed 's/">//g' | sort | uniq) ; do echo "$i" ; echo $I ; I=$(($I + 1)) ; brave-browser "$i" ; done
